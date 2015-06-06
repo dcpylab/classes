32a98de202ec8edfe479859434085fec6d9a2adf
Rami Chowdhury <rami@socialtables.com>
2015-06-06 11:15:10 -0400

    Finish off rps

--- 2015-04-18/rps.py
+++ 2015-04-18/rps.py
@@ -24,27 +24,27 @@ winners = {
 }
 
 def rps(choice1, choice2):
-    if choice1 == choice2:
-        print("Tie!")
-        return
+  if choice1 == choice2:
+    print("Tie!")
+    return
 
-    if choice1 not in winners:
-        print("Choice 1 not valid, dude!")
-        return
+  if choice1 not in winners:
+    print("Choice 1 not valid, dude!")
+    return
 
-    if choice2 not in winners:
-        print("Choice 2 not valid, dude!")
-        return
+  if choice2 not in winners:
+    print("Choice 2 not valid, dude!")
+    return
 
-    player_1_wins_against = winners[choice1]
-    player_2_wins_against = winners[choice2]
+  player_1_wins_against = winners[choice1]
+  player_2_wins_against = winners[choice2]
 
-    if choice2 in player_1_wins_against:
-        print("Player 1 wins")
-    elif choice1 in player_2_wins_against:
-        print("Player 2 wins")
-    else:
-        print("I don't even know what just happened")
+  if choice2 in player_1_wins_against:
+    print("Player 1 wins")
+  elif choice1 in player_2_wins_against:
+    print("Player 2 wins")
+  else:
+    print("I don't even know what just happened")
 
 
 # TEST CASES
