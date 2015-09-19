7c97dd9916eedf0a593444e18b34914eeae70380
Nathan Danielsen <nathan.danielsen@gmail.com>
2015-09-19 13:14:47 -0400

    removed tabs

--- 2015-09/starter.py
+++ 2015-09/starter.py
@@ -12,10 +12,10 @@ filename = 'daily_show_guests.csv'
 
 if not os.path.isfile(filename):
 
-	urllib.urlretrieve(url, 'daily_show_guests.csv')
-	# use requests if you have pip on your machine
+    urllib.urlretrieve(url, 'daily_show_guests.csv')
+    # use requests if you have pip on your machine
 
 else:
-	pass
+    pass
 
 print 'hello world!!!'
