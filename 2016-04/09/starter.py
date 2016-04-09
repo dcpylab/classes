ad6794ea6163a6c93c6dbfa2a90bf36c72a52c2f
Nathan Danielsen <nathan.danielsen@gmail.com>
2016-04-09 16:06:35 -0400

    added class code

--- 2016-04/09/starter.py
+++ 2016-04/09/starter.py
@@ -15,7 +15,7 @@ Analyzing Project Gutenberg's The Innocents Abroad, by Mark Twain (Samuel Clemen
 # https://www.gutenberg.org/ebooks/3176
 
 Feel free to reach out with questions
-nathan.danielsen@gmail.com 
+nathan.danielsen@gmail.com
 
 """
 # Constants
@@ -30,15 +30,18 @@ filename = 'innocents-abroad.txt'
 # Print out file contents to terminal
 # Inspect file structure and format
 
+with open(filename, 'r') as f:
+    book_data = f.read()
+    # print(book_data)
+
 
 # Questions:
 
-# 1. Using string slicing, what is the title of this book?
 
 
+# 2. How many characters (a,b,c,1,3,4) are in this txt file?
 
-
-# 2. How many characters are in this txt file?
+# print(len(book_data))
 
 
 
@@ -46,9 +49,77 @@ filename = 'innocents-abroad.txt'
 # 3. How many '\n' are in this text file? When is the first '\n'?
 
 
+# print(unicode(book_data))
+# print(help(type("hello")))
+# print(book_data.count('\n'))
+# book_data_short = book_data[:75]
+# print(book_data_short.count('g'))
+# print(book_data.count('the'))
+# str.count(sub, start= 0,end=len(string))
+
+
+# book_data_short = book_data[:75]
+
+
+# print(repr(book_data_short))
+
+by_author = book_data.find(', by')
+end_of_first_line = book_data.find('\n')
+# print( book_data[by_author:end_of_first_line])
+
+
+# Project Gutenberg's The Innocents Abroad, by Mark Twain (Samuel Clemens)
+
+def get_title_author(book_data):
+    # first the end of the first line
+    end_of_first_line = book_data.find('\n')
+
+    # find the Title
+    gutenberg_intro = "Gutenberg's "
+    title_start = book_data.find(gutenberg_intro) + len(gutenberg_intro)
+
+    # find the author
+
+    by_str = ', by '
+    by_author = book_data.find(by_str)
+    position_author = by_author + len(by_str)
+
+    title  = book_data[title_start: by_author ]
+    author = book_data[position_author:end_of_first_line]
+
+    return {'title': title, 'author': author }
+
+# print(get_title_author(book_data))
+
+dummy_book_data = "Project Gutenberg's God Emperor of Dume, by Frank Herbert\n\n"
+
+# print(get_title_author(dummy_book_data))
+
+
+# 1. Using string slicing, what is the title of this book?
+
+# book_data.index
+
+
+# Input:
+    # The entire open book file -- or a shorter slice of the book
+
+
+# Output:
+    # Title of the book
+
 
 
 # 4. Pick a few words and figure out how to count their number of incidences?
+
+# print(book_data.count('the'))
+#
+book_data_lower = book_data.lower()
+#
+# print(book_data_lower.count('the'))
+book_data_lower = book_data_lower.replace('\n', '')
+
+book_words_list = book_data_lower.split(' ')
 
 
 
@@ -56,7 +127,7 @@ filename = 'innocents-abroad.txt'
 ## Challenge
 # 5. How many words are in this text?
 
-
+print(len(book_words_list))
 
 
 # 6. Lower case all of the text. How many words now?
@@ -65,7 +136,12 @@ filename = 'innocents-abroad.txt'
 
 
 ## Extra Challenge
-# 7. Can you figure out how to remove all punctuation and special characters from this text file? What is the unique work count now? (hint: use a set )
+# 7. Can you figure out how to remove all punctuation and special characters from this text file?
+# 7b. What is the unique word count now? (hint: use a set )
+
+unique_items = set(book_words_list) ## for unique words
+
+myword_dict = {} # for counting words
 
 
 
@@ -83,6 +159,6 @@ if __name__ == '__main__':
         # use requests if you have pip on your machine
         print('File: %s downloaded' % filename)
     else:
-        print('File: %s present and ready to go!' % filename)
+        # print('File: %s present and ready to go!' % filename)
 
         pass
