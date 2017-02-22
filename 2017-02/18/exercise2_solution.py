25211dcd50c79d080fa60ac431464016182c9d73
Natalia C. Clementi <natyclementi@gmail.com>
2017-02-22 09:51:01 -0500

    Start script for exercise 2-add statement

---
+++ 2017-02/18/exercise2_solution.py
@@ -0,0 +1,24 @@
+"""
+Introduction to Python
+DATE - 18 February 2017
+Instructor - Nathan Danielsen @nate_somewhere
+https://github.com/ndanielsen/beginning-python/
+"""
+
+"""
+Exercise 1:
+
+Using the first paragraph:
+1. How many words are in the first paragraph?
+2. How many sentences are in the first paragraph?
+3. How many times is the word 'Obama' is in the first paragraph?
+4. If you remove all of the punctuation and lower case all text, how many words?
+
+
+Note: For a guided and step by step solution explanation in jupyter notebooks go to:
+https://github.com/ncclementi/dcpython_exercises/blob/master/02-18-17_application_real_data.ipynb
+
+You will also find useful links and some extra tips to try in your script.
+"""
+
+
