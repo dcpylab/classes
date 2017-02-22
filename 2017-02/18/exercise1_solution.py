4e71064098fbd2284f4fef59310594d80e5e19b2
Naty Clementi <natyclementi@gmail.com>
2017-02-21 22:10:07 -0500

    Start solution for exercise 1 - 02-18-17.
    Writing statement of the exercise.

---
+++ 2017-02/18/exercise1_solution.py
@@ -0,0 +1,20 @@
+"""
+Introduction to Python
+DATE - 18 February 2017
+Instructor - Nathan Danielsen @nate_somewhere
+https://github.com/ndanielsen/beginning-python/
+"""
+
+"""
+Exercise 1:
+
+From the following header extract:
+1. Extract the title
+2. Extract the introduction
+3. Extract the author
+4. Extract the photographer
+
+*Hint*: Google something called repr and check what `print(repr(header))` tells you.
+"""
+
+
