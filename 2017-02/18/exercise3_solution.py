1a22db4c01076eee25699609e9defe007c188365
Natalia C. Clementi <natyclementi@gmail.com>
2017-02-24 15:42:00 -0500

    Start solution exercise3 -  add header

---
+++ 2017-02/18/exercise3_solution.py
@@ -0,0 +1,26 @@
+"""
+Introduction to Python
+DATE - 18 February 2017
+Instructor - Nathan Danielsen @nate_somewhere
+https://github.com/ndanielsen/beginning-python/
+"""
+
+"""
+## Eercise 3: Advanced
+
+1. Open the article_part_one.txt with python.
+2. How many words are in part one?
+3. How many sentences are in part one?
+4. Which words follow 'black' and 'white' in the text? Which ones are used the most for each?
+
+*Hint*: Google open file with python
+
+Note: For a guided and step by step solution explanation in jupyter notebooks go to:
+https://github.com/ncclementi/dcpython_exercises/blob/master/02-18-17_application_real_data.ipynb
+
+You will also find useful links and some extra tips to try in your script.
+
+In this case because it was the advanced exercise, I introduce new things that 
+we didin't see on the class. hope you have fun!
+"""
+
